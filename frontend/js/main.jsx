d94d2211ce32ce4709762db0c707bdf680893268
aghabidareh <aghabidareh@gmail.com>
2026-05-28 20:19:12 +0330

    fix: showing up UI

--- frontend/js/main.jsx
+++ frontend/js/main.jsx
@@ -1,13 +1,18 @@
+import "@vitejs/plugin-react/preamble";
 import "../css/main.css";
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 
+const el = document.getElementById("app");
+const page = el?.dataset.page ? JSON.parse(el.dataset.page) : null;
+
 createInertiaApp({
+  page,
   resolve: (name) => {
     const pages = import.meta.glob("../pages/**/*.jsx");
     return pages[`../pages/${name}.jsx`]();
   },
-  setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />);
+  setup({ el: appElement, App, props }) {
+    createRoot(appElement).render(<App {...props} />);
   },
 });
